71a2f517b55c2f380cd4ee512a3a0124ed4a3c09
blac <baptiste.lac@insa-lyon.fr>
2017-12-27 02:59:01 +0100

    R-script to display fitness graphs

---
+++ graphs.r
@@ -0,0 +1,34 @@
+rm(list=ls())
+library('ggplot2')
+setwd("~/Documents/INSA/BC/Projet/Evolution-Simulation")
+
+D = read.table('history.csv', sep='\t', header=T)
+
+fitness = D$fitness
+time = D$time
+event = D$event
+kept = D$kept
+
+
+seg = data.frame(x0=time[1:(length(time)-1)],
+                 y0=fitness[1:(length(fitness)-1)],
+                 x1=time[2:length(time)],
+                 y1=fitness[2:length(fitness)],
+                 events=event[2:length(event)])
+
+plotA = ggplot() + 
+  geom_segment(aes(x=x0,
+                   y=y0,
+                   xend=x1,
+                   yend=y1,
+                   colour=events),
+               data=seg,
+               size=1.5) +
+  geom_point(aes(x=time, y=fitness, shape=factor(kept)), size=2) + 
+  xlab('Time') + 
+  ylab('Fitness') + 
+  ggtitle('Fitness evolution and events') + 
+  theme(plot.title = element_text(lineheight=1, face="bold"))
+
+plotA
+
