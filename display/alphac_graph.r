f36b30c0ec8b37604d1c734d24268df9584b67f7
blac <baptiste.lac@insa-lyon.fr>
2018-01-14 18:32:06 +0100

    params et autres

---
+++ display/alphac_graph.r
@@ -0,0 +1,24 @@
+rm(list=ls())
+
+library('plyr')
+library('ggplot2')
+
+
+DA = read.table('alphac.csv', sep='\t', header=T)
+
+xy1 = ggplot(DA, aes(x=alphac)) +
+  geom_line(aes(y=maxfitness, 
+                colour='Max'),
+            size=1.5) +
+  geom_line(aes(y=meanfitness,
+                colour='Mean'),
+            size=1.5) +
+  theme(legend.position='right') +
+  xlab('Alpha_C') +
+  ylab('Fitness') +
+  ggtitle('Mean and max fitness vs. Alpha_C') +
+  scale_color_discrete(name='Legend')
+
+pdf('plt_meanmax.pdf', width=8, height=4)
+xy1
+dev.off()
